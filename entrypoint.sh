63f29e51849fb0c81843542228284b731d2ad176
刘广峥 <liuguangzheng@qiyi.com>
2019-01-25 17:19:57 +0800

    创建/data/share/.android文件夹

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,5 @@ USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u ${USER_ID} -o -c "" -m user
-exec gosu user "ln -s /data/share/.android ${HOME}/.android && $@"
+exec gosu user "ln -s /data/share/.android ${HOME}/.android"
+exec gosu user "$@"
